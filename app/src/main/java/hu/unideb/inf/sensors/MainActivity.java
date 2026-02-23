8505ffc3613b26788f97f9bcd48d55b3186e95b9
Dr. John H. Watson <john.watson@inf.unideb.hu>
2026-02-23 10:50:20 +0100

    Layout 2

--- app/src/main/java/hu/unideb/inf/sensors/MainActivity.java
+++ app/src/main/java/hu/unideb/inf/sensors/MainActivity.java
@@ -3,6 +3,7 @@ package hu.unideb.inf.sensors;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -31,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         sensorsTextView = findViewById(R.id.sensorsTextView);
+        //sensorsTextView.setMovementMethod(new ScrollingMovementMethod());
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
